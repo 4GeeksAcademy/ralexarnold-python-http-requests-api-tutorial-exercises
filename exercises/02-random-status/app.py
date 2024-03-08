f60fda7f2d004fad1333a56dcc54257c99a95a64
Robert (Alex) Arnold <138836491+RAlexArnold@users.noreply.github.com>
2024-03-08 04:04:29 +0000

    completing exercises

--- exercises/02-random-status/app.py
+++ exercises/02-random-status/app.py
@@ -1,3 +1,24 @@
 import requests
 
+import requests
+
 response = requests.get("https://assets.breatheco.de/apis/fake/sample/random-status.php")
+
+status_code = str(response.status_code)
+
+print("The response status is: "+str(response.status_code))
+
+if status_code == '404':
+    print('The URL you asked is not found')
+
+elif status_code == '503':
+    print('Unavailable right now')
+
+elif status_code == '200':
+    print('Everything went perfect')
+
+elif status_code == '400':
+    print('Something is wrong on the request params')
+
+else:
+    print('Anything Else')
